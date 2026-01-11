752bf0cb82f6392d30c4c3461e2314f272fbff5e
MargaretTechworld <margarettechworld@gmail.com>
2026-01-11 19:05:09 +0000

    add welcome screen, login and signup page

--- src/components/NafraDataForm.js
+++ src/components/NafraDataForm.js
@@ -1,10 +1,10 @@
 import './styles/NafraDataForm.css';
 
 export default function NafraDataForm() {
-    return (
-        <div>
-            <h1>Welcome to Nafra Data Form portal</h1>
-            <p>Please carefully fill out the form below to submit your data.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to Nafra Data Form portal</h1>
+      <p>Please carefully fill out the form below to submit your data.</p>
+    </div>
+  );
 }
